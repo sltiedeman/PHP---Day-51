9d9625a08a9034625b90b9e9801b367373628541
sltiedeman <sltiedeman@gmail.com>
2016-01-04 13:51:44 -0500

    First PHP Project

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu|Oswald|Kameron">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="scripts.js" type="text/javascript"></script>
+</head>
+<body>
+
+<div id="total-wrapper">
+	<div id="social-media">
+		<div class="text">
+			<img src="http://pq.b5z.net/zirw/317/i/u/10099375/i/menu/qb877.gif">
+			Feedback
+		</div>
+		<div id="icons">
+			<img src="http://q.b5z.net/zirw/h495b/i/t/w/integration/social/icons/fcc1/tw_32x32.png">
+			<img  src="http://q.b5z.net/zirw/h495b/i/t/w/integration/social/icons/fcc1/fb_32x32.png">
+			<img src="http://q.b5z.net/zirw/h495b/i/t/w/integration/social/icons/fcc1/ig_32x32.png">
+		</div>
+		<div class="text">
+			<img src="http://pq.b5z.net/zirw/317/i/u/10099375/i/menu/qb658.gif">
+			<span>Prepare for Your Adventure</span>
+		</div>
+	</div>
+
+	<div id="banner">
+		<div id="header-holder">
+			<img src="header.png">
+		</div>
+		<div id="menu">
+			<ul>
+				<li>Home</li>
+				<li>About Us</li>
+				<li>Programs</li>
+				<li>Leadership</li>
+				<li>Contact Us</li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="main-content-wrapper">
+		<div id="main-content">
+			<div id="left">
+				<h1> <img style="display:inline" src="http://pq.b5z.net/zirw/317/i/u/10099375/i/menu/tico1.png">About Sojourn </h1>
+				<hr></hr>
+				<img id="sojourn-pic" src="sojourn.jpg">
+				<div class="text-left">
+					Our lives take us on many journeys; with our careers, families, sports teams, schools, etc...  At Sojourn, our desire is to provide a safe and fun environment to Sojourn from these life journeys for a brief period of time in order to reflect, gain new insight, and enter back into our journeys with new perspective.
+				</div>
+				<div class="picture-right">
+					<img src="http://pq.b5z.net/i/u/10099375/i/Fox_5_Video_of_Sojourn.png">
+				</div>
+				<hr class="rule"></hr>
+				
+				<div class="text-left">
+					<h1>Location </h1>
+					Most Sojourn programming takes place on the property of Perimeter church in John's Creek, GA. The property is located at the corner of Old Alabama Rd. and Hwy 141/Medlock Bridge Rd. 
+
+If location is a challenge, ask us about off-site adventures and portable programs.
+				</div>
+				<div class="picture-right">
+					<img src="http://pq.b5z.net/i/u/10099375/i/Get_Directions_to_Ropes_Course_Button.png">
+				</div>
+
+				<hr class="rule"></hr>
+				
+				<div class="text-left">
+					<h1>ACCT Membership </h1>
+					The Association for Challenge Course Technology is recognized as the leading professional resource for those who create safe and effective challenge course programs that facilitate learning and positive change in individuals, groups, and communities. We are an organizational member of ACCT and our products and services are independent crom those offered by ACCT.  We are not an agent of ACCT nor does the ACCT warrant, endorse, or approve any product or service offered by us.  Sojourn operates under the standards set by ACCT.  All of our staff members are training according to ACCT standards and our key staff members have advanced ACCT certification.
+				</div>
+				<div class="picture-right">
+					<img src="http://pq.b5z.net/i/u/10099375/i/ACCT_Logo_Button.png">
+				</div>
+
+
+				<hr class="rule"></hr>
+				<div id="pricing">
+					<h1>Pricing </h1>
+					Pricing varies depending on the length of your adventure, size of your group, and the activities necessary to attain certain group goals and objectives.  For full pricing and further details about booking a course, please contact the Sojourn office.
+					<p>
+					Note:  Sojourn currently focuses exclusively on team building with pre-existing groups and therefore does not allow individual sign-ups for zip lining, birthday parties, or similar outings.
+					</p>
+				</div>
+				
+
+
+
+			</div>
+			<div id="right">
+				<img src="http://pq.b5z.net/i/u/10099375/i/Get_a_Quote.png">
+				<img src="http://pq.b5z.net/i/u/10099375/i/Photo_Gallery_Button.png">
+				<img src="http://pq.b5z.net/i/u/10099375/i/video_tour_button.png">
+				<img src="http://pq.b5z.net/i/u/10099375/i/Prepare_for_Your_Event_Sidebar_Button.png"> 
+			</div>
+		</div>	
+	</div>
+
+	<div id="footer">
+		<ul>
+			<li>9500 Medlock Bridge Rd.</li>
+			<li>|</li>
+			<li>Johns Creek, GA 30097</li>
+			<li>|</li>
+			<li>Phone: 678.405.2106</li>
+			<li>|</li>
+			<li>Email: sojourn@perimeter.org</li>
+		</ul>
+		<img id="staff-login" alt="Ropes Course Staff Login" src="http://q.b5z.net/i/u/10099375/i/staff_login_button_footer.png">
+		<img alt="Association for Challenge Course Technology" src="http://q.b5z.net/i/u/10099375/i/acct_logo_footer.png">
+	</div>
+	
+</div>	
+
+</body>
+</html>
---
+++ scripts.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	var height = $('#main-content').height();
+	$('#right').css('height', height + 'px');
+})
