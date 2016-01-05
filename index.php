03292e0204bac5cd46746d9a938a198e251939c5
sltiedeman <sltiedeman@gmail.com>
2016-01-05 09:34:44 -0500

    Added a form

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
 <?php
+	$connection = mysql_connect('127.0.0.1', 'phpland', 'test');
+	if (!$connection) {
+		die('Could not connect: ' . mysql_error());
+	}
+	$db_selected = mysql_select_db('phpland', $connection);
+	if (!db_selected) {
+		die ('Can\'t use phpland : ' . mysql_error());
+	}
+	// else{
+	// 	print "success!!";
+	// }
+	//in this case phpland is the name of our db and not the user!
+	// $query = "SELECT * FROM about WHERE section = 'header'";
+	$query = "SELECT * FROM about";
+	//We have a var called query with our query 
+	$result = mysql_query($query);
+	//We now have a mysql object called result!
+	print mysql_error();
+	// while ($row = mysql_fetch_assoc($result)){
+	// 	$rows[] = $row;
+	// }
+	while ($row = mysql_fetch_assoc($result)){
+		$section = $row["section"];
+		$rows[$section] = $row['content'];
+	}
 
+	// $header_content = $rows[0]['content'];
+
+
+	//this will print off our error if one happens
+	$row = mysql_fetch_assoc($result);
+	// print "<pre>";
+	// print_r ($row['id']);
+	// print "</pre>";
+	$header_content = $row['content'];
+
+	$feedback = 'Feedback';
+	$span = '<span>Prepare for Your Adventure</span>';
+	$home = '<li>Home</li>';
+	$about = '<li>About Us</li>';
+	$programs = '<li>Programs</li>';
+	$leadership = '<li>Leadership</li>';
+	$contact = '<li>Contact Us</li>';
+	$textleft1 = 'Our lives take us on many journeys; with our careers, families, sports teams, schools, etc...  At Sojourn, our desire is to provide a safe and fun environment to Sojourn from these life journeys for a brief period of time in order to reflect, gain new insight, and enter back into our journeys with new perspective.';
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +62,7 @@
 	<div id="social-media">
 		<div class="text">
 			<img src="http://pq.b5z.net/zirw/317/i/u/10099375/i/menu/qb877.gif">
-			Feedback
+			<?php print $feedback ?>
 		</div>
 		<div id="icons">
 			<img src="http://q.b5z.net/zirw/h495b/i/t/w/integration/social/icons/fcc1/tw_32x32.png">
@@ -28,7 +71,7 @@
 		</div>
 		<div class="text">
 			<img src="http://pq.b5z.net/zirw/317/i/u/10099375/i/menu/qb658.gif">
-			<span>Prepare for Your Adventure</span>
+			<?php print $span ?>
 		</div>
 	</div>
 
@@ -38,11 +81,11 @@
 		</div>
 		<div id="menu">
 			<ul>
-				<li>Home</li>
-				<li>About Us</li>
-				<li>Programs</li>
-				<li>Leadership</li>
-				<li>Contact Us</li>
+				<?php print $home ?>
+				<?php print $about ?>
+				<?php print $programs ?>
+				<?php print $leadership ?>	
+				<?php print $contact ?>		
 			</ul>
 		</div>
 	</div>
@@ -54,7 +97,7 @@
 				<hr></hr>
 				<img id="sojourn-pic" src="sojourn.jpg">
 				<div class="text-left">
-					Our lives take us on many journeys; with our careers, families, sports teams, schools, etc...  At Sojourn, our desire is to provide a safe and fun environment to Sojourn from these life journeys for a brief period of time in order to reflect, gain new insight, and enter back into our journeys with new perspective.
+					<?php print $rows['about'] ?>
 				</div>
 				<div class="picture-right">
 					<img src="http://pq.b5z.net/i/u/10099375/i/Fox_5_Video_of_Sojourn.png">
@@ -62,10 +105,8 @@
 				<hr class="rule"></hr>
 				
 				<div class="text-left">
-					<h1>Location </h1>
-					Most Sojourn programming takes place on the property of Perimeter church in John's Creek, GA. The property is located at the corner of Old Alabama Rd. and Hwy 141/Medlock Bridge Rd. 
-
-If location is a challenge, ask us about off-site adventures and portable programs.
+					<?php print '<h1>Location </h1>'; ?>
+					<?php print $rows['header']; ?>
 				</div>
 				<div class="picture-right">
 					<img src="http://pq.b5z.net/i/u/10099375/i/Get_Directions_to_Ropes_Course_Button.png">
@@ -75,7 +116,7 @@ If location is a challenge, ask us about off-site adventures and portable progra
 				
 				<div class="text-left">
 					<h1>ACCT Membership </h1>
-					The Association for Challenge Course Technology is recognized as the leading professional resource for those who create safe and effective challenge course programs that facilitate learning and positive change in individuals, groups, and communities. We are an organizational member of ACCT and our products and services are independent crom those offered by ACCT.  We are not an agent of ACCT nor does the ACCT warrant, endorse, or approve any product or service offered by us.  Sojourn operates under the standards set by ACCT.  All of our staff members are training according to ACCT standards and our key staff members have advanced ACCT certification.
+					<?php print $rows['acct']; ?>
 				</div>
 				<div class="picture-right">
 					<img src="http://pq.b5z.net/i/u/10099375/i/ACCT_Logo_Button.png">
@@ -85,10 +126,8 @@ If location is a challenge, ask us about off-site adventures and portable progra
 				<hr class="rule"></hr>
 				<div id="pricing">
 					<h1>Pricing </h1>
-					Pricing varies depending on the length of your adventure, size of your group, and the activities necessary to attain certain group goals and objectives.  For full pricing and further details about booking a course, please contact the Sojourn office.
-					<p>
-					Note:  Sojourn currently focuses exclusively on team building with pre-existing groups and therefore does not allow individual sign-ups for zip lining, birthday parties, or similar outings.
-					</p>
+					<?php print $rows['pricing']; ?>
+
 				</div>
 				
 
@@ -105,15 +144,7 @@ If location is a challenge, ask us about off-site adventures and portable progra
 	</div>
 
 	<div id="footer">
-		<ul>
-			<li>9500 Medlock Bridge Rd.</li>
-			<li>|</li>
-			<li>Johns Creek, GA 30097</li>
-			<li>|</li>
-			<li>Phone: 678.405.2106</li>
-			<li>|</li>
-			<li>Email: sojourn@perimeter.org</li>
-		</ul>
+		<?php print $rows['address']; ?>
 		<img id="staff-login" alt="Ropes Course Staff Login" src="http://q.b5z.net/i/u/10099375/i/staff_login_button_footer.png">
 		<img alt="Association for Challenge Course Technology" src="http://q.b5z.net/i/u/10099375/i/acct_logo_footer.png">
 	</div>
